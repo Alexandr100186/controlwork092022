a5b1501853255357ad48ba37bee758a35876b42b
DESKTOP-LLAL946\devil <mr.alkov@mail.ru>
2022-09-15 23:43:55 +0700

    addprogram

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+
+void SecondArraySearch3(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        } 
+    }
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+SecondArraySearch3(array1, array2);
+PrintArray(array1);
+PrintArray(array2);
--- README.md
+++
@@ -1,1 +0,0 @@
-# controlwork092022
